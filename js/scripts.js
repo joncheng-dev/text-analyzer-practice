cf0fc487aef5c4b6d52ad65870c0361aca3d25f8
joncheng-dev <joncheng.dev@gmail.com>
2023-08-15 11:02:43 -0700

    wordCounter funtion pass 5th test -- for counting words only, not nubmers
    co-authored-by: Ian Bravo <bravo.ian@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,14 @@
 
 function wordCounter(text) {
     if (text.trim().length === 0) {
-      return 0;
+        return 0;
     }
     let wordCount = 0;
     const textArray = text.split(" ");
-    textArray.forEach(function(word) {
-      wordCount++;
+    textArray.forEach(function (element) {
+        if (!Number(element)) {
+            wordCount++;
+        }
     });
     return wordCount;
-  }
+}
