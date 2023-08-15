b8ee6e5d4b47f9b77efb075e91bd48428b3aea0f
joncheng-dev <joncheng.dev@gmail.com>
2023-08-15 11:24:08 -0700

    function numberOfOccurrencesInText pass 3rd - 6th tests
    co-authored-by: Ian Bravo <bravo.ian@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,8 +15,12 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-    if (word === text) {
-      return 1;
-    }
-    return 0;
-  }
+    const textArray = text.split(" ");
+    let wordCount = 0;
+    textArray.forEach(function(element) {
+      if (element.toLowerCase().includes(word.toLowerCase())) {
+        wordCount++;
+      }
+    });
+    return wordCount;
+}
